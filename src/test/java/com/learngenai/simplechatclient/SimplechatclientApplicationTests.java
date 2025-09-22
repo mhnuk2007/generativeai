89fce0248d2ef12df09356837bb2d500231cf90d
Mohan Lal <mhnuk2007@gmail.com>
2025-09-22 13:50:59 +0500

    Create and add simple generative ai chat application

---
+++ src/test/java/com/learngenai/simplechatclient/SimplechatclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngenai.simplechatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplechatclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
